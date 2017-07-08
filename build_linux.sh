2108db6cba4cecd4a37817f22ea933fab97e6a44
Tad seldovia <tadeze@live.com>
2017-07-08 08:41:43 -0700

    local

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,8 @@
 
+
+
 echo "Build command for iforest"
+
 if [ $# -eq 0 ]
 then
 	echo "No argument supplied. Defualt build command will be in release mode."
@@ -17,8 +20,14 @@ then
 		fi
 		cd $buildType
 		echo "Building in $buildType mode"
-		cmake -DCMAKE_BUILD_TYPE=$buildType ..
-		make
+
+	export CC=/usr/local/common/gcc-4.9.0/bin/gcc
+	export CXX=/usr/local/common/gcc-4.9.0/bin/g++
+	export LDFLAGS=/usr/local/common/gcc-4.9.0/lib64/
+
+ 
+	cmake -DCMAKE_BUILD_TYPE=$buildType ..
+	make
 else
 		echo " Please type either Release or Debug as an argument."
 fi
